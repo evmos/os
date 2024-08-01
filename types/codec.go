8a371fe4fd15e472c361f30e474bb4022be7f947
MalteHerrmann <malte@evmos.org>
2024-08-01 13:22:51 +0200

    add evm related types

---
+++ types/codec.go
@@ -0,0 +1,29 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+package types
+
+import (
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/types/tx"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+)
+
+// RegisterInterfaces registers the tendermint concrete client-related
+// implementations and interfaces.
+func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
+	registry.RegisterImplementations(
+		(*authtypes.AccountI)(nil),
+		// TODO: uncomment after moving into migrations for EVM version
+		// &EthAccount{},
+	)
+	registry.RegisterImplementations(
+		(*authtypes.GenesisAccount)(nil),
+		// TODO: uncomment after moving into migrations for EVM version
+		// &EthAccount{},
+	)
+	registry.RegisterImplementations(
+		(*tx.TxExtensionOptionI)(nil),
+		&ExtensionOptionsWeb3Tx{},
+		&ExtensionOptionDynamicFeeTx{},
+	)
+}
