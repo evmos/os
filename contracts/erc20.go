ab675cc0c7765d16f2c8ea2833268c9e9f5b57fb
MalteHerrmann <42640438+MalteHerrmann@users.noreply.github.com>
2024-08-06 13:58:30 +0200

    imp(contracts): Add contracts directory and compilation script (#24)

---
+++ contracts/erc20.go
@@ -0,0 +1,30 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+
+package contracts
+
+import (
+	_ "embed"
+
+	evmtypes "github.com/evmos/evmos/v19/x/evm/types"
+	contractutils "github.com/evmos/os/contracts/utils"
+)
+
+var (
+	// ERC20MinterBurnerDecimalsJSON are the compiled bytes of the ERC20MinterBurnerDecimalsContract
+	//
+	//go:embed solidity/ERC20MinterBurnerDecimals.json
+	ERC20MinterBurnerDecimalsJSON []byte
+
+	// ERC20MinterBurnerDecimalsContract is the compiled erc20 contract
+	ERC20MinterBurnerDecimalsContract evmtypes.CompiledContract
+)
+
+func init() {
+	var err error
+	if ERC20MinterBurnerDecimalsContract, err = contractutils.ConvertHardhatBytesToCompiledContract(
+		ERC20MinterBurnerDecimalsJSON,
+	); err != nil {
+		panic(err)
+	}
+}
